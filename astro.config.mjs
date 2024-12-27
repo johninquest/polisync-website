f5373281f59bed723c82ab555a53d9ac5e712c1d
Jinq <43062557+johninquest@users.noreply.github.com>
2024-12-27 18:06:35 +0100

    added google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'; 
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-    devToolbar: {
-        enabled: false,
-    }
+  devToolbar: {
+    enabled: false,
+  },
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
